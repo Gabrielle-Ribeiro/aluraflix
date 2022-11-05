2330efd57964b7ff5bc724d12d40e732ddb60e6d
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-11-05 16:08:54 -0300

    Cria classe para videos

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    titulo = models.CharField(max_length=100, blank=False, null=False)
+    descricao = models.CharField(max_length=1000, blank=False, null=False)
+    url = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        return self.titulo
