cd4196cf3742f3462931fa202137fbaa68001b58
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-11-05 16:40:31 -0300

    Cria view set para Video

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from api.models import Video
+from api.serializer import VideoSerializer
 
-# Create your views here.
+
+class VideoViewSet(viewsets.ModelViewSet):
+    """Exibe todos os vídeos"""
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
