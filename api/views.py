d7074ee54ce3b18ded3d3307d70e4ff092b182f0
Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
2022-11-05 17:12:02 -0300

    Adiciona mensagem personaliza para requisição Delete

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from django.http import Http404
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from api.models import Video
 from api.serializer import VideoSerializer
 
@@ -8,3 +10,16 @@ class VideoViewSet(viewsets.ModelViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+        except Http404:
+            return Response({
+                "message": "Erro! Não foi possível encontrar o vídeo."
+            },
+                status=status.HTTP_404_NOT_FOUND)
+        return Response({
+            "message": "Deletado com sucesso!"
+        },
+            status=status.HTTP_200_OK)
